3598d90fb8537388e2cbb3be5be0d17f5a8ad8e9
sergica97 <sergiocaballeroalejo@gmail.com>
2019-01-03 16:02:06 +0100

    Añadimos a esta clase la funcionalidad para que mediante el botón calcular nos diga si el número introducido es primo o no.

---
+++ PrcticaT4/app/src/main/java/com/studium/prcticat4/NumerosPrimos.java
@@ -0,0 +1,48 @@
+package com.studium.prcticat4;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class NumerosPrimos extends AppCompatActivity {
+
+    Button btnCalcular;
+    EditText edtNumeroEntero;
+    TextView txtResultadoPrimos;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_numeros_primos);
+
+        btnCalcular = findViewById(R.id.btnCalcular);
+        edtNumeroEntero = findViewById(R.id.edtNumeroEntero);
+        txtResultadoPrimos = findViewById(R.id.txtResultadoPrimos);
+        this.setTitle(R.string.numeros);
+
+        btnCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                int num = Integer.parseInt(edtNumeroEntero.getText().toString());
+                boolean prime = true;
+                for(int i = 2; i < num; i++) {
+                    if (num % i == 0) {
+                        prime = false;
+                        break;
+                    }
+                }
+                if (prime)
+                    txtResultadoPrimos.setText("Este número es primo");
+                else
+                    txtResultadoPrimos.setText("Este número NO es primo");
+            }
+        });
+    }
+
+}
+
