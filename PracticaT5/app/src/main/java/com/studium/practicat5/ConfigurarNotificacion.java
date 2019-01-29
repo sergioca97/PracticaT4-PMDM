de9e1d789fa463102a16e73fa649cefa0c252dd1
sergica97 <sergiocaballeroalejo@gmail.com>
2019-01-29 21:28:10 +0100

    Commit inicial para cargar el proyecto en el repositorio.

--- PracticaT5/app/src/main/java/com/studium/practicat5/ConfigurarNotificacion.java
+++ PracticaT5/app/src/main/java/com/studium/practicat5/ConfigurarNotificacion.java
@@ -1,13 +1,58 @@
 package com.studium.practicat5;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ConfigurarNotificacion extends AppCompatActivity {
+    AppCompatActivity ConfigurarNotificacion;
+    Button btnEnviar;
+    EditText edtTitulo, edtMensaje, edtTiempo;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configurar_notificacion);
+        ConfigurarNotificacion = this;
+        btnEnviar = findViewById(R.id.btnEnviar);
+        edtTitulo = findViewById(R.id.edtTitulo);
+        edtMensaje = findViewById(R.id.edtMensaje);
+        edtTiempo = findViewById(R.id.edtTiempo);
+        setTitle("Configurar Notificación");
+
+        btnEnviar.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                NotificationCompat.Builder mensaje = new NotificationCompat.Builder(ConfigurarNotificacion, "CH_ID");
+
+                Intent intent =  new Intent(ConfigurarNotificacion.this, ConfigurarNotificacion.class);
+
+                PendingIntent contIntent =  PendingIntent.getActivity(ConfigurarNotificacion.this, 0, intent, 0);
+
+                mensaje.setContentIntent(contIntent);
+
+                mensaje.setAutoCancel(true)
+                        .setDefaults(Notification.DEFAULT_ALL)
+                        .setWhen(System.currentTimeMillis())
+                        .setSmallIcon(android.R.drawable.star_on)
+                        .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher_background))
+                        .setContentText(edtTitulo.getText().toString())
+                        .setContentTitle(edtMensaje.getText().toString());
+
+                NotificationManager nM = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                nM.notify(1, mensaje.build());
+            }
+        });
     }
 }
--- PracticaT5/app/src/main/java/com/studium/practicat5/ConfigurarToast.java
+++ PracticaT5/app/src/main/java/com/studium/practicat5/ConfigurarToast.java
@@ -12,103 +12,83 @@ import android.widget.Toast;
 
 public class ConfigurarToast extends AppCompatActivity {
 
+    int valor = 0;
     Button btnMostrar;
     EditText edtHorizontal, edtVertical, edtTexto;
-    RadioGroup rndHorizontal, rndVertical;
     RadioButton radIzquierda, radMedio, radDerecha, radArriba, radCentro, radAbajo;
-    int numero = 0;
-    Boolean seleccionar = false;
 
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configurar_toast);
-
         btnMostrar = findViewById(R.id.btnMostrar);
         edtHorizontal = findViewById(R.id.edtHorizontal);
         edtVertical = findViewById(R.id.edtVertical);
         edtTexto = findViewById(R.id.edtTexto);
-        rndHorizontal = findViewById(R.id.rndHorizontal);
         radIzquierda = findViewById(R.id.radIzquierda);
         radMedio = findViewById(R.id.radMedio);
         radDerecha = findViewById(R.id.radDerecha);
-        rndVertical = findViewById(R.id.rndVertical);
         radArriba = findViewById(R.id.radArriba);
         radCentro = findViewById(R.id.radCentro);
         radAbajo = findViewById(R.id.radAbajo);
-
-        final Toast toast = Toast.makeText(getApplicationContext(), edtTexto.getText(), Toast.LENGTH_SHORT);
-        rndHorizontal.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                if (radIzquierda.isChecked() && radArriba.isChecked()) {
-                    toast.setGravity(Gravity.LEFT | Gravity.TOP, Integer.parseInt(edtHorizontal.getText().toString()), Integer.parseInt(edtVertical.getText().toString()));
-                } else if (radIzquierda.isChecked() && radCentro.isChecked()) {
-                    toast.setGravity(Gravity.LEFT | Gravity.CENTER_VERTICAL, Integer.parseInt(edtHorizontal.getText().toString()), Integer.parseInt(edtVertical.getText().toString()));
-                } else if (radIzquierda.isChecked() && radAbajo.isChecked()) {
-                    toast.setGravity(Gravity.LEFT | Gravity.BOTTOM, Integer.parseInt(edtHorizontal.getText().toString()), Integer.parseInt(edtVertical.getText().toString()));
-                } else if (radMedio.isChecked() && radArriba.isChecked()) {
-                    toast.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.TOP, Integer.parseInt(edtHorizontal.getText().toString()), Integer.parseInt(edtVertical.getText().toString()));
-                } else if (radMedio.isChecked() && radCentro.isChecked()) {
-                    toast.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.CENTER_VERTICAL, Integer.parseInt(edtHorizontal.getText().toString()), Integer.parseInt(edtVertical.getText().toString()));
-                } else if (radMedio.isChecked() && radAbajo.isChecked()) {
-                    toast.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.BOTTOM, Integer.parseInt(edtHorizontal.getText().toString()), Integer.parseInt(edtVertical.getText().toString()));
-                } else if (radDerecha.isChecked() && radArriba.isChecked()) {
-                    toast.setGravity(Gravity.RIGHT | Gravity.TOP, Integer.parseInt(edtHorizontal.getText().toString()), Integer.parseInt(edtVertical.getText().toString()));
-                } else if (radDerecha.isChecked() && radCentro.isChecked()) {
-                    toast.setGravity(Gravity.RIGHT | Gravity.CENTER_VERTICAL, Integer.parseInt(edtHorizontal.getText().toString()), Integer.parseInt(edtVertical.getText().toString()));
-                } else if (radDerecha.isChecked() && radAbajo.isChecked()) {
-                    toast.setGravity(Gravity.RIGHT | Gravity.BOTTOM, Integer.parseInt(edtHorizontal.getText().toString()), Integer.parseInt(edtVertical.getText().toString()));
-                }
-                toast.show();
-            }
-
-
-            });
-        rndVertical.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId)
-            {
-                if (radIzquierda.isChecked() && radArriba.isChecked()) {
-                    toast.setGravity(Gravity.LEFT | Gravity.TOP, Integer.parseInt(edtHorizontal.getText().toString()), Integer.parseInt(edtVertical.getText().toString()));
-                } else if (radIzquierda.isChecked() && radCentro.isChecked()) {
-                    toast.setGravity(Gravity.LEFT | Gravity.CENTER_VERTICAL, Integer.parseInt(edtHorizontal.getText().toString()), Integer.parseInt(edtVertical.getText().toString()));
-                } else if (radIzquierda.isChecked() && radAbajo.isChecked()) {
-                    toast.setGravity(Gravity.LEFT | Gravity.BOTTOM, Integer.parseInt(edtHorizontal.getText().toString()), Integer.parseInt(edtVertical.getText().toString()));
-                } else if (radMedio.isChecked() && radArriba.isChecked()) {
-                    toast.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.TOP, Integer.parseInt(edtHorizontal.getText().toString()), Integer.parseInt(edtVertical.getText().toString()));
-                } else if (radMedio.isChecked() && radCentro.isChecked()) {
-                    toast.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.CENTER_VERTICAL, Integer.parseInt(edtHorizontal.getText().toString()), Integer.parseInt(edtVertical.getText().toString()));
-                } else if (radMedio.isChecked() && radAbajo.isChecked()) {
-                    toast.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.BOTTOM, Integer.parseInt(edtHorizontal.getText().toString()), Integer.parseInt(edtVertical.getText().toString()));
-                } else if (radDerecha.isChecked() && radArriba.isChecked()) {
-                    toast.setGravity(Gravity.RIGHT | Gravity.TOP, Integer.parseInt(edtHorizontal.getText().toString()), Integer.parseInt(edtVertical.getText().toString()));
-                } else if (radDerecha.isChecked() && radCentro.isChecked()) {
-                    toast.setGravity(Gravity.RIGHT | Gravity.CENTER_VERTICAL, Integer.parseInt(edtHorizontal.getText().toString()), Integer.parseInt(edtVertical.getText().toString()));
-                } else if (radDerecha.isChecked() && radAbajo.isChecked()) {
-                    toast.setGravity(Gravity.RIGHT | Gravity.BOTTOM, Integer.parseInt(edtHorizontal.getText().toString()), Integer.parseInt(edtVertical.getText().toString()));
-                }
-                toast.show();
-            }
-        });
-
         btnMostrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v)
             {
+
+                if(edtHorizontal.getText().toString().isEmpty())
+                {
+                    edtHorizontal.setText("" + valor);
+                }
+                if(edtVertical.getText().toString().isEmpty())
+                {
+                    edtVertical.setText("" + valor);
+                }
                 if (!edtTexto.getText().toString().isEmpty())
                 {
-                    if(edtHorizontal.getText().toString().isEmpty())
-                    {
-                        edtHorizontal.setText("" +numero);
-                    }
-                    if(edtVertical.getText().toString().isEmpty())
-                    {
-                        edtVertical.setText("" +numero);
-                    }
+
                     Toast toast1 = Toast.makeText(getApplicationContext(), edtTexto.getText(), Toast.LENGTH_SHORT);
-            }
+                    if(radIzquierda.isChecked() && radArriba.isChecked())
+                    {
+                        toast1.setGravity(Gravity.LEFT|Gravity.TOP,Integer.parseInt(edtHorizontal.getText().toString()) ,Integer.parseInt(edtVertical.getText().toString()));
+                    }
+                    else if(radIzquierda.isChecked() && radCentro.isChecked())
+                    {
+                        toast1.setGravity(Gravity.LEFT|Gravity.CENTER_VERTICAL,Integer.parseInt(edtHorizontal.getText().toString()) ,Integer.parseInt(edtVertical.getText().toString()));
+                    }
+                    else if(radIzquierda.isChecked() && radAbajo.isChecked())
+                    {
+                        toast1.setGravity(Gravity.LEFT|Gravity.BOTTOM,Integer.parseInt(edtHorizontal.getText().toString()) ,Integer.parseInt(edtVertical.getText().toString()));
+                    }
+                    else if(radMedio.isChecked() && radArriba.isChecked())
+                    {
+                        toast1.setGravity(Gravity.CENTER_HORIZONTAL|Gravity.TOP,Integer.parseInt(edtHorizontal.getText().toString()) ,Integer.parseInt(edtVertical.getText().toString()));
+                    }
+                    else if(radMedio.isChecked() && radCentro.isChecked())
+                    {
+                        toast1.setGravity(Gravity.CENTER_HORIZONTAL|Gravity.CENTER_VERTICAL,Integer.parseInt(edtHorizontal.getText().toString()) ,Integer.parseInt(edtVertical.getText().toString()));
+                    }
+                    else if(radMedio.isChecked() && radAbajo.isChecked())
+                    {
+                        toast1.setGravity(Gravity.CENTER_HORIZONTAL|Gravity.BOTTOM,Integer.parseInt(edtHorizontal.getText().toString()) ,Integer.parseInt(edtVertical.getText().toString()));
+                    }
+                    else if(radDerecha.isChecked() && radArriba.isChecked())
+                    {
+                        toast1.setGravity(Gravity.RIGHT|Gravity.TOP,Integer.parseInt(edtHorizontal.getText().toString()) ,Integer.parseInt(edtVertical.getText().toString()));
+                    }
+                    else if(radDerecha.isChecked() && radCentro.isChecked())
+                    {
+                        toast1.setGravity(Gravity.RIGHT|Gravity.CENTER_VERTICAL,Integer.parseInt(edtHorizontal.getText().toString()) ,Integer.parseInt(edtVertical.getText().toString()));
+                    }
+                    else if(radDerecha.isChecked() && radAbajo.isChecked())
+                    {
+                        toast1.setGravity(Gravity.RIGHT|Gravity.BOTTOM,Integer.parseInt(edtHorizontal.getText().toString()) ,Integer.parseInt(edtVertical.getText().toString()));
+                    }
+                    toast1.show();
+                }
+                else
+                {
+                    edtTexto.setHint("Error escriba un texto");
+                }
             }
         });
     }
